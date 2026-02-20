33c2bcd12a70b75ec2bef319a53a5564878243a9
Dave G <dpg3k@virginia.edu>
2026-02-20 13:31:19 -0500

    Add the config helper

---
+++ lambda-common/config-helpers.go
@@ -0,0 +1,41 @@
+//
+//
+//
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func ensureSet(env string) (string, error) {
+	val, set := os.LookupEnv(env)
+
+	if set == false {
+		err := fmt.Errorf("environment variable not set: [%s]", env)
+		fmt.Printf("ERROR: %s\n", err.Error())
+		return "", err
+	}
+
+	return val, nil
+}
+
+func ensureSetAndNonEmpty(env string) (string, error) {
+	val, err := ensureSet(env)
+	if err != nil {
+		return "", err
+	}
+
+	if val == "" {
+		err := fmt.Errorf("environment variable is empty: [%s]", env)
+		fmt.Printf("ERROR: %s\n", err.Error())
+		return "", err
+	}
+
+	return val, nil
+}
+
+//
+// end of file
+//
