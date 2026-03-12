4dec6733c30527cb584050e43217eab363c04cf6
Dave G <dpg3k@virginia.edu>
2026-03-12 13:18:10 -0400

    Handle extra field and better logging

--- lambda-common/events.go
+++ lambda-common/events.go
@@ -10,7 +10,7 @@ import (
 func NewEventBus(eventBus string, eventSource string) (uvaaptsbus.UvaBus, error) {
 	// we will accept bad config and return nil quietly
 	if len(eventBus) == 0 {
-		fmt.Printf("INFO: Event bus is not configured, no telemetry emitted\n")
+		fmt.Printf("INFO: Event bus is not configured, no events emitted\n")
 		return nil, uvaaptsbus.ErrConfig
 	}
 
@@ -18,21 +18,27 @@ func NewEventBus(eventBus string, eventSource string) (uvaaptsbus.UvaBus, error)
 	return uvaaptsbus.NewUvaBus(cfg)
 }
 
-func publishWorkflowEvent(bus uvaaptsbus.UvaBus, eventName string, clientId string, submissionId string, bagId string) error {
+func publishWorkflowEvent(bus uvaaptsbus.UvaBus, eventName string, clientId string, submissionId string, bagId string, extra string) error {
 	if bus == nil {
 		return uvaaptsbus.ErrConfig
 	}
-	detail, _ := workflowPayload(submissionId, bagId)
+	detail, _ := workflowPayload(submissionId, bagId, extra)
 	ev := uvaaptsbus.UvaBusEvent{
 		EventName: eventName,
 		ClientId:  clientId,
 		Detail:    detail,
 	}
-	return bus.PublishEvent(&ev)
+
+	fmt.Printf("INFO: publishing [%v]\n", ev)
+	err := bus.PublishEvent(&ev)
+	if err != nil {
+		fmt.Printf("ERROR: publishing (%s)\n", err.Error())
+	}
+	return err
 }
 
-func workflowPayload(submissionId string, bagId string) (json.RawMessage, error) {
-	pl := uvaaptsbus.UvaWorkflowEvent{SubmissionId: submissionId, BagId: bagId}
+func workflowPayload(submissionId string, bagId string, extra string) (json.RawMessage, error) {
+	pl := uvaaptsbus.UvaWorkflowEvent{SubmissionId: submissionId, BagId: bagId, Extra: extra}
 	return pl.Serialize()
 }
 
