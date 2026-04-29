9199d97c8f82048b0bc56ec55a4d3e0b60dffb52
Dave G <dpg3k@virginia.edu>
2026-04-29 08:22:39 -0400

    Remove S3 purge, we might need these assets later so handle purge with an S3 rule

--- apt-submit-purge/process.go
+++ apt-submit-purge/process.go
@@ -26,8 +26,8 @@ func process(messageId string, messageSrc string, rawMsg json.RawMessage) error 
 	switch be.EventName {
 	case uvaaptsbus.EventSubmissionComplete: // submission complete and ready to purge
 	case uvaaptsbus.EventCommandSubmissionPurge: // submission complete and ready to purge
-	case uvaaptsbus.EventBagAccepted: // bag accepted by APT
-	case uvaaptsbus.EventCommandBagPurge: // bag complete and ready to purge
+	//case uvaaptsbus.EventBagAccepted: // bag accepted by APT
+	//case uvaaptsbus.EventCommandBagPurge: // bag complete and ready to purge
 	default:
 		fmt.Printf("WARNING: unexpected event type (%s), ignoring\n", be.EventName)
 		return nil
@@ -49,11 +49,11 @@ func process(messageId string, messageSrc string, rawMsg json.RawMessage) error 
 	}
 
 	// create our s3 helper client
-	s3Client, err := newS3Client()
-	if err != nil {
-		fmt.Printf("ERROR: creating s3 client (%s)\n", err.Error())
-		return err
-	}
+	//s3Client, err := newS3Client()
+	//if err != nil {
+	//	fmt.Printf("ERROR: creating s3 client (%s)\n", err.Error())
+	//	return err
+	//}
 
 	// assets in [bucket|cache]/<clientId>/<submissionId>/[bagId]...
 	pathPrefix := path.Join(be.ClientId, wf.SubmissionId)
@@ -66,16 +66,16 @@ func process(messageId string, messageSrc string, rawMsg json.RawMessage) error 
 	case uvaaptsbus.EventSubmissionComplete, uvaaptsbus.EventCommandSubmissionPurge:
 		fmt.Printf("INFO: purging assets for submission <%s>\n", wf.SubmissionId)
 
-	case uvaaptsbus.EventBagAccepted, uvaaptsbus.EventCommandBagPurge:
-		fmt.Printf("INFO: purging assets for bag <%s/%s>\n", wf.SubmissionId, wf.BagId)
-		pathPrefix = path.Join(pathPrefix, wf.BagId)
+		//case uvaaptsbus.EventBagAccepted, uvaaptsbus.EventCommandBagPurge:
+		//	fmt.Printf("INFO: purging assets for bag <%s/%s>\n", wf.SubmissionId, wf.BagId)
+		//	pathPrefix = path.Join(pathPrefix, wf.BagId)
 	}
 
 	// get a complete list of all the files included in the specified path
-	keys, err := s3Client.s3List(cfg.AssetBucket, pathPrefix)
-	if err != nil {
-		fmt.Printf("WARNING: listing S3 assets (%s), continuing\n", err.Error())
-	}
+	//keys, err := s3Client.s3List(cfg.AssetBucket, pathPrefix)
+	//if err != nil {
+	//	fmt.Printf("WARNING: listing S3 assets (%s), continuing\n", err.Error())
+	//}
 
 	efsDir := path.Join(cfg.AssetFilesystem, pathPrefix)
 	contents, err := os.ReadDir(efsDir)
@@ -84,15 +84,15 @@ func process(messageId string, messageSrc string, rawMsg json.RawMessage) error 
 	}
 
 	// purge the S3 assets
-	if len(keys) != 0 {
-		fmt.Printf("INFO: located %d assets in [s3://%s/%s]\n", len(keys), cfg.AssetBucket, pathPrefix)
-		err = purgeS3Assets(s3Client, cfg.AssetBucket, keys)
-		if err != nil {
-			fmt.Printf("WARNING: purging S3 assets (%s), continuing\n", err.Error())
-		}
-	} else {
-		fmt.Printf("WARNING: no S3 assets located\n")
-	}
+	//if len(keys) != 0 {
+	//	fmt.Printf("INFO: located %d assets in [s3://%s/%s]\n", len(keys), cfg.AssetBucket, pathPrefix)
+	//	err = purgeS3Assets(s3Client, cfg.AssetBucket, keys)
+	//	if err != nil {
+	//		fmt.Printf("WARNING: purging S3 assets (%s), continuing\n", err.Error())
+	//	}
+	//} else {
+	//	fmt.Printf("WARNING: no S3 assets located\n")
+	//}
 
 	// purge the cache
 	if len(contents) != 0 {
